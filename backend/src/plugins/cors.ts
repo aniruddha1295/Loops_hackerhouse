abdd446dd3267f892231deaf133c6dc5484a8559
Aniruddha <lahotiap@rknec.edu>
2026-05-10 23:00:37 +0530

    fix: allow all origins in CORS config to fix frontend requests

--- backend/src/plugins/cors.ts
+++ backend/src/plugins/cors.ts
@@ -5,8 +5,8 @@ import { config } from '../config/environment.js';
 
 export default fp(async function corsPlugin(fastify: FastifyInstance) {
   await fastify.register(cors, {
-    origin: [config.frontendUrl, 'http://localhost:5173'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
 }, {
